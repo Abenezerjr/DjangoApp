075e0b112e030a2a03a4ef71e49c59fd3328d0ff
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-11 00:09:12 +0300

    rendering some Database Modeling

--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import  views
 
 urlpatterns=[
-path("project/",views.project),
-path("home/",views.home),
-path("single-project/<str:pk>",views.singleProject),
+path("project/",views.project,name='projects'),
+# path("home/",views.home),
+path("single-project/<str:pk>",views.singleProject,name='project'),
 
 ]
 
--- project/views.py
+++ project/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
 
 # Create your views here.
-ListProject = {
-    'hiredive': 'this is a platform that coustomer hire a diveloper',
-    'EBC': 'this is a platform that coustomer hire a diveloper',
-
+ListProject = [
+    {
+    'id': '1',
+    'title': 'EBC',
+    'discription':'ethiopan backend developers'
+},
+    {
+    'id': '2',
+    'title': 'HireDev',
+    'discription':'this is a platform that coustomer hire a diveloper'
+},
+    {
+    'id': '3',
+    'title': 'blog',
+    'discription':'blog page personal page'
 }
-def home(request):
-    return render(request,'main.html')
+
+]
+
+
+# def home(request):
+#     return render(request,'main.html')
 
 def project(request):
-    return render(request,'project/project.html')
+    msg='hello im in from project'
+    number=30
+    conntext={
+        'listofproject':ListProject,
+        'msg':msg,
+        'number':number,
+    }
+    return render(request,'project/project.html',conntext)
 
 def singleProject(request,pk):
-    return render(request,'project/singleProject.html')
+    projectObj=None
+    for i in ListProject:
+        if i['id'] ==pk:
+            projectObj=i
+    context={
+        'projectObj':projectObj
+    }
+    return render(request,'project/singleProject.html',context)
