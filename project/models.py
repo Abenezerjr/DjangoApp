cf8a2147e31f02940ef6f30e9ac3bad5ae60ba34
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-12 13:40:39 +0300

    inside Django modles

--- project/models.py
+++ project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class Project(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField(null=True,blank=True)
+    demoLink=models.CharField(max_length=2000,blank=True,null=True)
+    sourceLink=models.CharField(max_length=2000,blank=True,null=True)
+    created=models.DateTimeField(auto_now_add=True)
+    id=models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return  self.title
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import  views
 
 urlpatterns=[
-path("project/",views.project,name='projects'),
+path("",views.project,name='projects'),
 # path("home/",views.home),
 path("single-project/<str:pk>",views.singleProject,name='project'),
 
