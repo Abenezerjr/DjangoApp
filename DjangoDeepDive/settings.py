44331223a8ce2bd7d102144f5449314802986070
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-17 12:57:21 +0300

    add a ui or theme instalition

--- DjangoDeepDive/settings.py
+++ DjangoDeepDive/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,8 @@ STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images') # tell were to upload pic sebmmited by user
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') #used wher we perpare upload in live srver
 
 
 
--- project/forms.py
+++ project/forms.py
@@ -1,4 +1,5 @@
 from  django.forms import ModelForm
+from django import forms
 from . models import Project
 
 
@@ -7,3 +8,18 @@ class AddProjectForm(ModelForm):
     class Meta:
         model=Project
         fields=['title','description','demoLink','sourceLink','tags','feature_image']
+
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(AddProjectForm,self).__init__(*args,**kwargs)
+        #
+        # self.fields['title'].widget.attrs.update({'class':'input','placeholder':'Add project title'})
+        # self.fields['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Add project description'})
+
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+            self.fields['feature_image'].widget.attrs.update({'class': 'btn btn--sub'})
