535a4506f4f02560390611dc541569ebcd95a287
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-22 10:12:26 +0300

    add skill Model and render profile

--- users/models.py
+++ users/models.py
@@ -25,3 +25,14 @@ class Profile(models.Model):
         return str(self.user.username)
 
 
+
+class Skill(models.Model):
+    owner=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    descreption = models.TextField(blank=True, null=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
 
 
 def profile(requset):
+    profiles=Profile.objects.all()
     context={
-
+     "profiles":profiles
     }
     return render(requset,'users/profile.html',context)
