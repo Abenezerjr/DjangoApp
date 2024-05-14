f5801e3e6a54bf28ee55e646dcc9eeb9a92cd72c
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-14 12:33:18 +0300

    working with JWT

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,39 @@
-
 from rest_framework import serializers
-from project.models import  Project
+from project.models import  Project , Tag ,Review
+from users.models import Profile
 
 
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """
+    serializer for chailed class
+    """
+    class Meta:
+        model=Review
+        fields='__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields='__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 class ProjectSerializer(serializers.ModelSerializer):
-     class Meta:
+    owner=ProfileSerializer(many=False)
+    tags=TagSerializer(many=True)
+    reviews=serializers.SerializerMethodField()
+    class Meta:
          model=Project
          fields='__all__'
+
+    def get_reviews(self,obj):
+        reviews=obj.review_set.all()
+        serializer=ReviewSerializer(reviews,many=True)
+        return serializer.data
+
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('',views.getRoutes),
     path('projects/', views.get_projects),
     path('project/<str:pk>/', views.get_project),
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # semiple token git using JWT
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Refrash Token to usinge time sesstion
 
 ]
